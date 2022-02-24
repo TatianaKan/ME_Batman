d9887ba8d29419369494cfe189bbf8c18bf15c35
TatianaKan <klanukt@gmail.com>
2022-02-24 16:31:26 +0300

    Добавлено аудио и иконка

--- js/main.js
+++ js/main.js
@@ -1,22 +1,64 @@
-const sliderThumbs = new Swiper('.slider-thumbs', {
-  loop: true,
-  spaceBetween: 20,
-  slidesPerView: 3,
-  centeredSlides: true,
-  loopedSlides: 5
+//burger
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+  navigation.classList.add('navigation-active');
 });
 
-sliderThumbs.on('click', (swiper)=> {
-  swiper.slideTo(swiper.clickedIndex)
-})
-
-const sliderMain = new Swiper('.slider-main', {
-  loop: true,
-  // thumbs: {
-  //   swiper: sliderThumbs 
-  // },
-  loopedSlides: 5
+navigationClose.addEventListener('click', () => {
+  navigation.classList.remove('navigation-active');
 });
 
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+
+///music
+try {
+  const mute = document.querySelector('.mute__checkbox');
+  const audio = new Audio('audio/audio/waterTower.mp3');
+
+  const checkMute = () => {
+    if (mute.checked) {
+      audio.play();
+    } else {
+      audio.pause();
+    };
+  }
+  mute.addEventListener('change', checkMute);
+} catch {
+
+}
+////slider
+try {
+  const sliderThumbs = new Swiper('.slider-thumbs', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 3,
+    centeredSlides: true,
+    loopedSlides: 5
+  });
+
+  sliderThumbs.on('click', (swiper) => {
+    swiper.slideTo(swiper.clickedIndex)
+  })
+
+  const sliderMain = new Swiper('.slider-main', {
+    loop: true,
+    // thumbs: {
+    //   swiper: sliderThumbs 
+    // },
+    loopedSlides: 5
+  });
+
+  sliderThumbs.controller.control = sliderMain;
+  sliderMain.controller.control = sliderThumbs;
+
+  const videos = document.querySelectorAll('video');
+  sliderMain.on('slideChange', () => {
+    for (let i = 0; i < videos.length; i += 1) {
+      videos[i].pause();
+    }
+  });
+}
+catch {
+}
